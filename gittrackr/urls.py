195e730e2b3e7ec01c04030e7507e794872185aa
DavidCh <david.chandra0501@gmail.com>
2024-01-05 01:03:14 +0700

    vercel

--- gittrackr/urls.py
+++ gittrackr/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("__reload__", include("django_browser_reload.urls")),
@@ -12,3 +14,5 @@ urlpatterns = [
 
     path('create_folder/', views.create_folder, name='create_folder'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
