0edc51ff0fe8ecb3b5796001b1b5ceacbee65a1c
DavidCh <david.chandra0501@gmail.com>
2024-01-05 00:57:43 +0700

    vercel

--- gittrackr/settings.py
+++ gittrackr/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 import os
 
 load_dotenv()
@@ -140,7 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
