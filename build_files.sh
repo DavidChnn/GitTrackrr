e6743ef81495be014d09eaa7bf4a0c7a5d4f1c43
DavidCh <david.chandra0501@gmail.com>
2024-01-05 00:03:34 +0700

    vercel

--- build_files.sh
+++ build_files.sh
@@ -4,9 +4,6 @@ pip install python-dotenv
 # install depedencies
 pip install -r requirements.txt
 
-# install tailwindCSS
-python3.9 manage.py tailwind install
-
 # migrate database
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
